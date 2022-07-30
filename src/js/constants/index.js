510308fc029f6546c8e522c028cb3272b6730f6c
jisoo-log <jisoolee.dev@gmail.com>
2022-07-30 21:02:52 +0900

    feat: 등수와 상금 내용 추가

--- src/js/constants/index.js
+++ src/js/constants/index.js
@@ -1,9 +1,26 @@
 export const LOTTO = {
-  PRICE: 1000,
+  PRICE: 1_000,
   MAX_NUM: 45,
+  MIN_NUM: 1,
   LENGTH: 6,
 };
 
 export const ERR_MSG = {
-  NOT_MULTIPLE_OF_1000: '로또 구입 금액을 1,000원 단위로 입력해 주세요.',
+  NOT_MULTIPLE_OF_1000: "로또 구입 금액을 1,000원 단위로 입력해 주세요.",
+};
+
+export const PLACE = {
+  FIRST: 0,
+  SECOND: 1,
+  THIRD: 2,
+  FOURTH: 3,
+  FIFTH: 4,
+};
+
+export const PRIZE = {
+  [PLACE.FIRST]: 2_000_000_000,
+  [PLACE.SECOND]: 30_000_000,
+  [PLACE.THIRD]: 1_500_000,
+  [PLACE.FOURTH]: 50_000,
+  [PLACE.FIFTH]: 5_000,
 };
